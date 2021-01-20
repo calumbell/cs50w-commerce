e25c8baba92217614b0db6c8fa76724720592429
Calum Bell <calumabell@googlemail.com>
2021-01-20 16:00:29 +0000

    created auction models

--- auctions/admin.py
+++ auctions/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import User, Auction, Bid, Comment
 
 # Register your models here.
+admin.site.register(User)
+admin.site.register(Auction)
+admin.site.register(Bid)
+admin.site.register(Comment)
--- auctions/models.py
+++ auctions/models.py
@@ -3,4 +3,35 @@ from django.db import models
 
 
 class User(AbstractUser):
-    pass
+    def __str__(self):
+        return f"{self.username}"
+
+class Auction(models.Model):
+    item_name = models.CharField(max_length=64)
+    item_description = models.CharField(max_length=800)
+    start_bid = models.FloatField()
+    start_time = models.DateTimeField()
+    end_time = models.DateTimeField()
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name="auctions")
+
+    def __str__(self):
+        return f"Auction #{self.id}: {self.name} ({self.user.username})"
+
+class Bid(models.Model):
+    amount = models.FloatField()
+    time = models.DateTimeField()
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name="bids")
+    auction = models.ForeignKey(Auction, on_delete=models.CASCADE, related_name="bids")
+
+    def __str__(self):
+        return f"Bid #{self.id}: {self.amount} on {self.auction.item_name} by {self.user.username}"
+
+
+class Comment(models.Model):
+    message = models.CharField(max_length=255)
+    time = models.DateTimeField()
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name="comments")
+    auction = models.ForeignKey(Auction, on_delete=models.CASCADE, related_name="comments")
+
+    def __str__(self):
+        return f"Comment #{self.id}: {self.user.username} on {self.auction.item_name}: {self.message}"
